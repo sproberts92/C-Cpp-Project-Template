6c5b7d03255336412225182f928331eefbec18a1
Samuel Roberts <sproberts92@outlook.com>
2016-07-03 03:00:25 +0200

    Make INSTALL.bat more generic
    Remove specific MulticoreBSP related setup

--- INSTALL.bat
+++ INSTALL.bat
@@ -1,33 +1,29 @@
-echo Cloning submodules...
-git submodule update --init
-echo Cloning submodules... Done.
+@echo off
+
+REM echo Cloning submodules...
+REM git submodule update --init
+REM echo Cloning submodules... Done.
 
 echo Setting compiler environment variables...
 call vcvarsall amd64
 echo Done.
 
-echo Compiling getopt library...
-pushd ext\getopt
-nmake /f nmakefile
-popd
-echo Compiling getopt library... Done.
-
-echo Compiling pthreads library...
-pushd ext\pthreads-win32\sources\pthreads-w32-2-9-1-release
-nmake clean VC
-popd
-echo Compiling pthreads-win32 library... Done.
-
-echo Compiling multicorebsp-for-c library... 
-pushd ext\multicorebsp-for-c
-nmake /f nmakefile
-popd
-echo Compiling multicorebsp-for-c library... 
+REM echo Compiling [...] library... 
+REM pushd ext\[...]
+REM nmake /f [...]
+REM popd
+REM echo Compiling [...] library... Done.
 
 echo removing .gitignore...
+
 pushd src
 del /f /q .gitignore
 popd
+
+pushd ext
+del /f /q .gitignore
+popd
+
 echo Done.
 
 echo Install complete.
